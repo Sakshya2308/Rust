07d2778de9bef4ed4903f42ec50c281b101ef723
Sakshya2308 <87920148+Sakshya2308@users.noreply.github.com>
2023-02-06 16:52:00 +0530

    Update generatingRandomNumber.rs

--- generatingRandomNumber.rs
+++ generatingRandomNumber.rs
@@ -7,6 +7,6 @@ use std::io::{self, Read};
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let random_num = rand::thread_rng().gen_range(1..101);
+    let random_num = rand::thread_rng().gen_range(1..101); //generate random number between [1,101)
     println!("Random: {}", random_num);
 }
