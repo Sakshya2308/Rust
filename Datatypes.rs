dd234239f776e713dc86d3e860ef7a228f4fd4f9
Sakshya2308 <87920148+Sakshya2308@users.noreply.github.com>
2023-02-06 16:41:03 +0530

    Create Datatypes.rs

---
+++ Datatypes.rs
@@ -0,0 +1,16 @@
+#![allow(unused)]
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io::{self, Read};
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+fn main() {
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    let age: &str = "47";
+    let mut age: u32 = age.trim().parse().expect("Age wasn't assigned a numer");
+    age = age + 1;
+    println!("I'm {} and I want ${}", age, ONE_MIL);
+}
