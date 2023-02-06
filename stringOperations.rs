8eb67d8470ed9987b65cd4b8c139b983024cc675
Sakshya2308 <87920148+Sakshya2308@users.noreply.github.com>
2023-02-06 17:38:44 +0530

    Create stringOperations.rs

---
+++ stringOperations.rs
@@ -0,0 +1,24 @@
+#![allow(unused)]
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io::{self, Read};
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+fn main() {
+    let st3 = String::from("x r t b h k k a m c");
+    let mut v1: Vec<char> = st3.chars().collect(); // collect characters from the string and store them in vector
+    v1.sort();
+    v1.dedup(); //remove the duplicates
+    for char in v1 {
+        println!("{}", char);
+    }
+    let st4: &str = "Random string";
+    let mut st5: String = st4.to_string();
+    println!("{}", st5);
+    let byte_arr1 = st5.as_bytes();
+    let st6 = &st5[0..6];
+    println!("String length : {} and the string {}", st6.len(), st6);
+    st5.clear();
+}
