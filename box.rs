e4efebd315fba14d2bd4fcf4cee58f62d988212a
Sakshya2308 <87920148+Sakshya2308@users.noreply.github.com>
2023-02-09 00:28:20 +0530

    Create box.rs

---
+++ box.rs
@@ -0,0 +1,26 @@
+fn main() {
+    // box pointer - reference to an address in memory
+    struct TreeNode<T> {
+        pub left: Option<Box<TreeNode<T>>>, 
+        pub right: Option<Box<TreeNode<T>>>, 
+        pub key: T, 
+    }
+    impl <T> TreeNode<T> {
+        pub fn new(key: T) -> Self {
+            TreeNode {left: None, right: None, key, }
+        }
+    
+    pub fn left(mut self, node: TreeNode<T>) -> Self {
+        self.left = Some(Box :: new(node));
+        self
+    }
+    pub fn right(mut self, node: TreeNode<T>) -> Self {
+        self.right = Some(Box :: new(node));
+        self
+    }
+    }
+    let node1 = TreeNode::new(1)
+    .left(TreeNode::new(2))
+    .right(TreeNode::new(3));
+
+}
